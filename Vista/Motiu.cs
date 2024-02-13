1fdac2b44b231e3439fc0f58b618a2c39d6d6ca8
eric <eric.requena.7e7@itb.cat>
2024-02-13 13:58:10 +0100

    Add project files.

---
+++ Vista/Motiu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Vista
+{
+    public class Motiu
+    {
+        private string motius;
+
+
+  
+        public string Motius { get { return motius; } set { motius = value; } }
+
+
+
+        public Motiu(string motiu)
+        {
+            Motius = motiu;
+        }
+
+        public void GetMotiuInfo()
+        {
+            Console.WriteLine($"Motius = {Motius}");
+        }
+    }
+}
---
+++ Vista/Pacient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Vista
+{
+    public class Pacient
+    {
+        private int dni;
+        private string nom_cognoms;
+        private int telefon;
+
+        public int DNI { get { return dni; } set { dni = value; } } 
+
+        public string Nom_cognom {  get { return nom_cognoms; } set { nom_cognoms = value; } }
+
+        public int Telefon { get { return telefon; } set { telefon = value; } }
+
+        public Pacient(int dni, string nom_cognoms, int telefon)
+        {
+            DNI = dni;
+            Nom_cognom = nom_cognoms;
+            Telefon = telefon;
+        }
+
+        public void GetPacientInfo()
+        {
+            Console.WriteLine($"DNI: {DNI}, Nom_cognom = {Nom_cognom}, Telefon = {Telefon}");
+        }
+    }
+}
