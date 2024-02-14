ef22ea04ab9ecb924d20c619ad0ad96399177eb7
eric <eric.requena.7e7@itb.cat>
2024-02-14 12:47:57 +0100

    Ejercicio Actualizado

--- Vista/Program.cs
+++ Vista/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Vista;
+namespace MySpace
+{
+    public class  MyClass
+    {
+        public static void Main()
+        {
+            Pacient MolAno = new Pacient(12345678, "Mol Ano", 666666666);
+            MolAno.GetPacientInfo();
+            Motiu motiu1 = new Motiu("Dolor de cap");
+            motiu1.GetMotiuInfo();
+            Propietari Yo = new Propietari("Eric", "Persona", 18, true);
+            Yo.GetPropietariInfo();
+        }
+        
+    }
+}
--- Vista/Propietari.cs
+++ Vista/Propietari.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace Vista
 {
-    internal class Propietari
+    public class Propietari
     {
+        private string nom;
+        private string raça;
+        private int edat;
+        private bool vacunat;
+
+        public string Nom { get { return nom; } set { nom = value; } }
+        public string Raça { get { return raça; } set { raça = value; } }
+        public int Edat { get { return edat; } set { edat = value; } }
+        public bool Vacunat { get { return vacunat; } set { vacunat = value; } }
+
+        public Propietari(string nom, string raça, int edat, bool vacunat)
+        {
+            Nom = nom;
+            Raça = raça;
+            Edat = edat;
+            Vacunat = vacunat;
+        }
+
+        public void GetPropietariInfo()
+        {
+            Console.WriteLine($"Nom: {Nom}, Raça = {Raça}, Edat = {Edat}, Vacunat = {Vacunat}");
+        }
     }
 }
